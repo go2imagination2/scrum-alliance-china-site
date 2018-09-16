17648247c0e6fe27be15e6fe3328e936d963593d
JackyShen <mebusw@163.com>
2018-09-16 22:20:10 +0800

    clear files and links to resource files

---
+++ docs/_scrum/js/Common/Messenger/Messenger.js
@@ -0,0 +1,34 @@
+﻿/////////////////////////////////////////
+//Eric Filonowich - Scrum Alliance
+//Messaging controller class
+////////////////////////////////////////
+var Messenger = function () {
+    /*
+    Send function
+
+    */
+    Send = function (referenceObjFunction, instanceObj) {
+        if (this[referenceObjFunction]) {
+            for (var i = 0; i < this[referenceObjFunction].length; i++) {
+                this[referenceObjFunction][i](instanceObj);
+            }
+        }
+    };
+
+    /*
+    Subscribe function
+
+    */
+    Subscribe = function (referenceObjFunction, implementationFunction) {
+        if (!this[referenceObjFunction]) {
+            this[referenceObjFunction] = [];
+        }
+        this[referenceObjFunction].push(implementationFunction);
+    };
+
+    return {
+        Send: Send,
+        Subscribe: Subscribe
+    };
+
+}();
